2b0878093db9468d77cb3107204c4d645683b5a1
Jerry Simon <seryjemon@gmail.com>
2022-06-27 15:43:34 -0600

    completed lab - week 3

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -17,14 +17,34 @@ afterEach(() => {
 });
 
 
-
-
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
+  
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByRole("textbox", {name: /Due Date/i})
+  const button = screen.getByRole('button', {name: /Add/i});
+
+  fireEvent.change(inputTask, { target: { value: "History"}});
+  fireEvent.change(inputDate, { target: { value: "12/30/2023"}});
+  fireEvent.click(button);
+
+  fireEvent.change(inputTask, { target: { value: "task"}});
+  fireEvent.change(inputDate, { target: { value: "06/20/2021"}});
+  fireEvent.click(button);
+
+  fireEvent.change(inputTask, { target: { value: "task"}});
+  fireEvent.change(inputDate, { target: { value: "09/20/2022"}});
+  fireEvent.click(button);
+
+  const check = screen.getByText(/History/i);
+  expect(check).toBeInTheDocument();
+  
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+
+
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
